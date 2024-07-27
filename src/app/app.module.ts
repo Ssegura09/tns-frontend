eec0552c97c36fd9a8ca645501ff13640a60cc99
Stephanie <stephaniemsegura09@gmail.com>
2024-07-27 09:52:22 -0500

    added navbar component and set up routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,37 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component'; // Import the NavBar component
 import { UserService } from './services/user.service';
+
+// Import the generated components
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+// Define your routes here
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Add your components
+  { path: 'about', component: AboutComponent }, // Add your components
+  { path: 'contact', component: ContactComponent }, // Add your components
+  // Add other routes as needed
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    // other components
+    NavBarComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    // other modules
+    RouterModule.forRoot(routes)
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
